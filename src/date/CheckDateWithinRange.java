783f8172baa9604b45843d74869e01a3db50ec62
= <=>
2018-04-04 12:34:51 +0530

    added class to check date in a given range

---
+++ src/date/CheckDateWithinRange.java
@@ -0,0 +1,96 @@
+package date;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CheckDateWithinRange {
+	
+	public static void main(String[] args) {
+		//stem.out.println(isThisDateWithin3MonthsRange("31/01/2018", "dd/MM/yyyy"));
+		
+		System.out.println(isThisDateBefore6monthsOfCurrentDate("5-Oct-2017", "dd-MMM-yyyy"));
+
+	}
+
+	private static boolean isThisDateBefore6monthsOfCurrentDate(String dateToValidate,
+			String dateFormat) {
+		SimpleDateFormat sdf =  new SimpleDateFormat( dateFormat);
+		sdf.setLenient(false);
+		
+		try{
+			
+			Date date = sdf.parse(dateToValidate);
+			Calendar currentDateBefore6Months =  Calendar.getInstance();
+			currentDateBefore6Months.add(Calendar.MONTH, -6 );
+			
+			System.out.println("\n\ncurrentDate : "
+					+ Calendar.getInstance().getTime());
+			System.out.println("currentDateBefore3Months : "
+					+ currentDateBefore6Months.getTime());
+			System.out.println("dateToValidate : " + dateToValidate);
+			
+			if ( date.after(currentDateBefore6Months.getTime())) 
+				return true;
+			return false;
+			
+		}catch(ParseException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		
+		
+	}
+
+	public static boolean isThisDateWithin3MonthsRange(String dateToValidate,
+			String dateFromat) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat(dateFromat);
+		sdf.setLenient(false); // https://dzone.com/articles/techtip-use-setlenient-method
+		try {
+
+			// if not valid, it will throw ParseException
+			Date date = sdf.parse(dateToValidate);
+
+			// current date after 3 months
+			Calendar currentDateAfter3Months = Calendar.getInstance();
+			currentDateAfter3Months.add(Calendar.MONTH, 3);
+
+			// current date before 3 months
+			Calendar currentDateBefore3Months = Calendar.getInstance();
+			currentDateBefore3Months.add(Calendar.MONTH, -3);
+
+			/*************** verbose ***********************/
+			System.out.println("\n\ncurrentDate : "
+					+ Calendar.getInstance().getTime());
+			System.out.println("currentDateAfter3Months : "
+					+ currentDateAfter3Months.getTime());
+			System.out.println("currentDateBefore3Months : "
+					+ currentDateBefore3Months.getTime());
+			System.out.println("dateToValidate : " + dateToValidate);
+			/************************************************/
+
+			if (date.before(currentDateAfter3Months.getTime())
+					&& date.after(currentDateBefore3Months.getTime())) {
+
+				// ok everything is fine, date in range
+				return true;
+
+			} else {
+
+				return false;
+
+			}
+
+		} catch (ParseException e) {
+
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+
+}
